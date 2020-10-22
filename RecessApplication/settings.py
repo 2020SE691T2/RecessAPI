ccf80fb9de8dacb1ea6dc84b6baa7a9b20b34761
Jonathan McDaniel <JonathanTMcDaniel@gmail.com>
2020-10-22 18:01:18 -0400

    Update settings.py

--- RecessApplication/settings.py
+++ RecessApplication/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,3 +158,5 @@ REST_FRAMEWORK = {
     ]
 }
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
