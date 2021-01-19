2685b751c3531a285906eb128919a9c9f7cc0251
Jeffrey Durelli <jfd77@drexel.edu>
2021-01-18 22:58:48 -0500

    Merge fixes

--- RecessApplication/views.py
+++ RecessApplication/views.py
@@ -100,7 +100,7 @@ class ClassViewSet(viewsets.ModelViewSet):
 
         if not instance.meeting_link:
             data = { "topic": instance.class_name + "-" + instance.section}
-            meeting_json = ClassViewSet.zoom_proxy.create_meeting(data)
+            meeting_json = self.get_zoom_proxy().create_meeting(data)
             meeting = meeting_json.data
 
             serializer.save(meeting_link=meeting["join_url"])
